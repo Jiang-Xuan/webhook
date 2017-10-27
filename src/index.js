6ba8b9872c4ee0d1a28067688c0e90230a9ac3fc
jiangxuan <jiangxuan@dxy.cn>
2017-10-28 01:22:44 +0800

    process.yml

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World\n\n');
 })
 
 module.exports.server = server
