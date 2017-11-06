8d234383ca40ec33e7cc552758244b00df6fa00b
jiangxuan <jiangxuan@dxy.cn>
2017-11-06 18:37:20 +0800

    remoteDev

--- constants/server.js
+++ constants/server.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 
-module.exports.HOSTNAME = process.env.NODE_ENV === 'dev' ? '127.0.0.1' : '172.104.118.145'
+const { NODE_ENV } = process.env
+
+module.exports.HOSTNAME = NODE_ENV === 'dev' && NODE_ENV !== 'remoteDev' ? '127.0.0.1' : '172.104.118.145'
 module.exports.PORT = 3000
-const WEBHOOKCONFIGFILE =  process.env.NODE_ENV === 'dev' ? '/Users/jiangxuan/loveTech/auto-manage-system/webhook/webhook/config.json' : ''
+const WEBHOOKCONFIGFILE = NODE_ENV === 'dev' ? '/Users/jiangxuan/loveTech/auto-manage-system/webhook/webhook/config.json' : ''
 
 module.exports.GETWEBHOOKCONFIG = () => {
   return new Promise((resolve, reject) => {
